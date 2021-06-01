54819ebb81cafc6848fd52893cc7f9f7f41a6e23
于洋 <1317030038@qq.com>
2021-06-01 15:15:35 +0800

    快捷键

--- YY.Screenshot.win/Form1.Designer.cs
+++ YY.Screenshot.win/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace YY.Screenshot.win
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,17 +53,28 @@ namespace YY.Screenshot.win
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 414);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +82,7 @@ namespace YY.Screenshot.win
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- YY.Screenshot.win/Form1.cs
+++ YY.Screenshot.win/Form1.cs
@@ -9,39 +9,30 @@ namespace YY.Screenshot.win
         public Form1()
         {
             InitializeComponent();
-            screenshot = new Screenshot();
-
-
+         
             hkl.Add(hotkey1);
-            hkl.Add(hotkey2);
-
             hkl.HotkeyPressed += Hkl_HotkeyPressed;
+
+            label1.Text = "快捷键:" + hotkey1;
         }
 
         private HotkeyListener hkl = new HotkeyListener();
         private Hotkey hotkey1 = new Hotkey(Keys.Control | Keys.Shift, Keys.J);
-        private Hotkey hotkey2 = new Hotkey("Control+Shift+D4");
-        private Screenshot screenshot;
-
-
+        private Screenshot screenshot = new Screenshot();
 
         private void Hkl_HotkeyPressed(object sender, HotkeyEventArgs e)
         {
             if (e.Hotkey == hotkey1)
             {
-                Image image = this.screenshot.start(this, false, true);
+                Image image = this.screenshot.start(this, true, true);
                 this.pictureBox1.Image = image;
-            }
-
-            if (e.Hotkey == hotkey2)
-            {
-                MessageBox.Show($"Second hotkey '{hotkey2}'was pressed.");
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Image image = this.screenshot.start(this, true, true);
+            this.pictureBox1.Image = image;
         }
     }
 }
